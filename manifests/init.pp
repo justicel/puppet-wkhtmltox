23d8ca91983207c1b007f8ed9c24c3b2def9181f
Justice London <jlondon@syrussystems.com>
2015-07-13 13:33:35 -0700

    You know... actually update the download URL.

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,7 +50,7 @@ class wkhtmltox (
   $packagetype     = $::wkhtmltox::params::packagetype,
   $provider        = $::wkhtmltox::params::provider,
   $required_pkgs   = $::wkhtmltox::params::required_pkgs,
-  $download_url    = 'http://iweb.dl.sourceforge.net/project/wkhtmltopdf',
+  $download_url    = 'http://download.gna.org/wkhtmltopdf',
   $wkhtml_filename = undef,
   $use_downloader  = true,
 ) inherits ::wkhtmltox::params {
